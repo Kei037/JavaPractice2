4102aada7f49a64e74dc8f580e61eec7bbcc45bf
Kei037 <juneway@hanmail.net>
2024-01-09 12:11:21 +0900

    commit test

--- src/ch_01/Method_01.java
+++ src/ch_01/Method_01.java
@@ -1,28 +1,31 @@
+package ch_01;
+
 import java.util.Scanner;
 
-public class Method_02 {
-    static Scanner sc = new Scanner(System.in);
+public class Method_01 {
     static int getMax(int a, int b, int c) {
         int max = a;
         if (b > max) max = b;
         if (c > max) max = c;
         return max;
     }
-    static void inputData(int[] height, int[] weight, int[] age) {
-        for (int i = 0; i < 3; i++) {
-            System.out.print("[" + (i + 1) + "] ");
-            System.out.print("신장:");     height[i] = sc.nextInt();
-            System.out.print("    체중:"); weight[i] = sc.nextInt();
-            System.out.print("    나이:"); age[i] = sc.nextInt();
-        }
-    }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         int[] height = new int[3];
         int[] weight = new int[3];
         int[] age = new int[3];
 
-        inputData(height, weight, age);
+        for (int i = 0; i < 3; i++) {
+            System.out.print("[" + (i + 1) + "] ");
+            System.out.print("신장:");
+            height[i] = sc.nextInt();
+            System.out.print("    체중:");
+            weight[i] = sc.nextInt();
+            System.out.print("    나이:");
+            age[i] = sc.nextInt();
+        }
 
         int maxHeight = getMax(height[0], height[1], height[2]);
         int maxWeight = getMax(weight[0], weight[1], weight[2]);
--- src/ch_01/Method_03.java
+++ src/ch_01/Method_03.java
@@ -1,3 +1,5 @@
+package ch_01;
+
 import java.util.Scanner;
 
 public class Method_03 {
